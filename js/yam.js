ae20d42818f3fc108a5dfc2f7a4e94f9aa9450ff
jarett-lee <jarett.lee.pi@gmail.com>
2018-07-17 01:14:23 -0400

    Refactor show dir into func

--- js/yam.js
+++ js/yam.js
@@ -4,43 +4,43 @@ const path = require('path')
 
 const fsPromises = require('./js/fsPromises')
 
-parentDir = '.'
-
-
-fs.readdir(parentDir, (err, files) => {
-  if (err) {
-    throw err
-  }
-
-  const len = files.length
-
-  const promises = files.map((file) => {
-    const fullFilePath = path.join(parentDir, file)
-    return fsPromises.stat(fullFilePath)
-  })
-  Promise.all(promises).then((stats) => {
-    const ul = document.getElementById('fileList')
-
-    for (let i = 0; i < len; i++) {
-      file = files[i]
-      stat = stats[i]
-
-      const span = document.createElement("span")
-      span.innerText = file
-
-      if (stat.isDirectory()) {
-        span.classList.add('dir')
-      } else if (stat.isFile()) {
-        span.classList.add('file')
-      } else {
-        throw new Error('Unexpected file type');
-      }
-
-      const li = document.createElement("li")
-      li.append(span)
-
-      ul.append(li)
+function showDir(parentDir) {
+  fs.readdir(parentDir, (err, files) => {
+    if (err) {
+      throw err
     }
-  })
 
-})
+    const len = files.length
+
+    const promises = files.map((file) => {
+      const fullFilePath = path.join(parentDir, file)
+      return fsPromises.stat(fullFilePath)
+    })
+    Promise.all(promises).then((stats) => {
+      const ul = document.getElementById('fileList')
+
+      for (let i = 0; i < len; i++) {
+        file = files[i]
+        stat = stats[i]
+
+        const span = document.createElement("span")
+        span.innerText = file
+
+        if (stat.isDirectory()) {
+          span.classList.add('dir')
+        } else if (stat.isFile()) {
+          span.classList.add('file')
+        } else {
+          throw new Error('Unexpected file type');
+        }
+
+        const li = document.createElement("li")
+        li.append(span)
+
+        ul.append(li)
+      }
+    })
+  })
+}
+
+showDir('.')
