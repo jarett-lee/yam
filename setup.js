9622cd6425f96dbcdf607285186d52338b5df685
jarett-lee <jarett.lee.pi@gmail.com>
2018-07-17 16:03:43 -0400

    Add a delay to removing the dir

--- setup.js
+++ setup.js
@@ -1,8 +1,16 @@
 
 const fse = require('fs-extra');
 
+function delay(t, v) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve.bind(null, v), t)
+  })
+}
+
 Promise.resolve().then(() => {
   return fse.remove('test-dir')
+}).then(() => {
+  return delay(100)
 }).then(() => {
   return fse.copy('test-images', 'test-dir')
 }).then(() => {
