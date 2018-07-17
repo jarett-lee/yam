af9dcc096fe61c2d6daad4d0350a3c527328574d
jarett-lee <jarett.lee.pi@gmail.com>
2018-07-17 00:38:41 -0400

    Get file stats

--- js/yam.js
+++ js/yam.js
@@ -1,15 +1,35 @@
 
 const fs = require('fs')
+const path = require('path')
 
-fs.readdir('.', (err, files) => {
+const fsPromises = require('./js/fsPromises')
+
+parentDir = '.'
+
+
+fs.readdir(parentDir, (err, files) => {
   if (err) {
     throw err
   }
 
-  fileList = document.getElementById('fileList')
-  for (file of files) {
-    const li = document.createElement("li")
-    li.innerText = file
-    fileList.append(li)
-  }
+  const len = files.length
+
+  const promises = files.map((file) => {
+    const fullFilePath = path.join(parentDir, file)
+    return fsPromises.stat(fullFilePath)
+  })
+  Promise.all(promises).then((stats) => {
+    const ul = document.getElementById('fileList')
+
+    for (let i = 0; i < len; i++) {
+      file = files[i]
+      stat = stats[i]
+
+      const li = document.createElement("li")
+      li.innerText = file
+      li.class = file
+      ul.append(li)
+    }
+  })
+
 })
